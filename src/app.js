a0230b0dac2ef73330a40957d20c34a86fd06638
EdissonMC <pattrick.adamss@gmail.com>
2025-06-19 11:56:57 -0500

    commit solo prueba

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
-app.get('/', (req, res) => {
+console.log("solo una prueba")
+app.get('/',   (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
